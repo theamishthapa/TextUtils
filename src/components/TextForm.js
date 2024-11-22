a569ce141a6eb710ec6d0c61beb1c6e411b4a528
amish thapa <thapaamish805@gmail.com>
2024-11-22 22:39:45 +0545

    added clear text and change bg color functionality

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,9 +1,16 @@
 import React, { useState } from "react";
 
 export default function TextForm(props) {
+  const [bgColor, setBgColor] = useState("white");
+
   const handleUpClick = () => {
     // console.log("uppercase was clicked" + text);
     let newText = text.toUpperCase();
+    setText(newText);
+  };
+
+  const handleLowClick = () => {
+    let newText = text.toLowerCase();
     setText(newText);
   };
 
@@ -11,23 +18,66 @@ export default function TextForm(props) {
     setText(event.target.value);
   };
 
-  const [text, setText] = useState("Enter text here");
+  const handleClearClick = () => {
+    setText("");
+  };
+
+  let clicked = true;
+  const handleChangeColor = () => {
+    if (clicked === true) {
+      document.getElementById("myBox").style.backgroundColor = "red";
+      clicked = false;
+    } else if (clicked === false) {
+      document.getElementById("myBox").style.backgroundColor = "white";
+      clicked = true;
+    }
+  };
+
+  const [text, setText] = useState("");
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          value={text}
-          onChange={handleOnChange}
-          id="myBox"
-          rows="6"
-        ></textarea>
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="6"
+          ></textarea>
+        </div>
+
+        <button className="btn btn-primary" onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
+
+        <button className="btn btn-primary mx-2" onClick={handleLowClick}>
+          Convert to Lowercase
+        </button>
+
+        <button className="btn btn-primary mx-2" onClick={handleClearClick}>
+          Clear Text
+        </button>
+
+        <button
+          className="btn btn-primary mx-2 my-2"
+          onClick={handleChangeColor}
+        >
+          Change Color
+        </button>
       </div>
 
-      <button className="btn btn-primary" onClick={handleUpClick}>
-        Convert to Uppercase
-      </button>
-    </div>
+      <div className="container my-3">
+        <h2>Your Text Summary</h2>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>Read in {text.split(" ").length * 0.008} minutes</p>
+
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
