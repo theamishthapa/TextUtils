edf7efefdab51ffd0b3f5a1017b29179de867752
amish thapa <thapaamish805@gmail.com>
2024-11-22 09:50:43 +0545

    created new component <TextForm> and used it to convert lower case to upper case

---
+++ src/components/TextForm.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const handleUpClick = () => {
+    // console.log("uppercase was clicked" + text);
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const [text, setText] = useState("Enter text here");
+  return (
+    <div>
+      <h1>{props.heading}</h1>
+      <div className="mb-3">
+        <textarea
+          className="form-control"
+          value={text}
+          onChange={handleOnChange}
+          id="myBox"
+          rows="6"
+        ></textarea>
+      </div>
+
+      <button className="btn btn-primary" onClick={handleUpClick}>
+        Convert to Uppercase
+      </button>
+    </div>
+  );
+}
